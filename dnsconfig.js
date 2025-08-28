fa7cf0fe19d75e5d1482419161657e6fca712940
CyreX <cyrex@rsmg-clan.de>
2025-08-28 11:15:18 +0200

    BS01

--- dnsconfig.js
+++ dnsconfig.js
@@ -76,6 +76,10 @@ D(
   REGISTRAR,
   MAIL01,
 
+  // NBG - BS01
+  A("152.53.204.169","bs01.cxsrv.de"),
+  AAAA("2a00:11c0:5f:3c0e::1","bs01.cxsrv.de"),
+
   // NBG - BS02
   A("bs02", "185.162.251.165"),
   AAAA("bs02", "2a03:4000:1a:9ba::1"),
@@ -120,6 +124,9 @@ D(
   "rsmg-clan.de",
   REGISTRAR,
   MAIL01,
+
+  // CNAME - BS01
+  CNAME("bs01","bs01.cxsrv.de."),
 
   // CNAME - BS02
   CNAME("bs02", "bs02.cxsrv.de."),
